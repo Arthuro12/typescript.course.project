e6143cff0e2027364c8576e9461b0b92751c561c
Athur Foadjo <arthurfoadjo@yahoo.com>
2023-10-25 08:05:02 +0200

    Revert "corrected path in "gitignore""
    This reverts commit d7b757135b55663d96e5bc480f602a7ce112e3c4.

--- Drag and Drop Project/generated/toolbox/toolbox.js
+++
@@ -1,8 +0,0 @@
-function validate(validationRules) {
-    let isValid = true;
-    if (validationRules.required) {
-        isValid = isValid && validationRules.value.trim().length !== 0;
-    }
-    return isValid;
-}
-export { validate };
