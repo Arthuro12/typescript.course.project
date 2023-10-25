ae131ded85b5297a21c06e2ef35e2c8d8a6592f3
Athur Foadjo <arthurfoadjo@yahoo.com>
2023-10-25 07:17:27 +0200

    Revert "first commit of the typescript course project"
    This reverts commit e72ea4e285bb8d88c957eb037f75e4cc1a275061.

--- Drag and Drop Project/generated/decorators/decorators.js
+++
@@ -1,9 +0,0 @@
-function boundThis(target, name, descriptor) {
-    return {
-        configurable: true,
-        get() {
-            return descriptor.value.bind(this);
-        },
-    };
-}
-export { boundThis };
